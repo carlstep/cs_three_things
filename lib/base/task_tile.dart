0fb73290e52a5cd82d337fb08ee7dbd62aa5d7dd
carlstep <carlstep333@me.com>
2024-08-12 04:53:49 +0700

    moved slidable to thetaskTile

--- lib/base/task_tile.dart
+++ lib/base/task_tile.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
@@ -43,133 +45,206 @@ class _TaskTileState extends State<TaskTile> {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          _isExpanded = !_isExpanded;
-        });
-      },
-      child: Card(
-        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15),
-          side: BorderSide(
-            width: .5,
-            color: Colors.grey.shade900,
-          ),
-        ),
-        // elevation set to 0
-        elevation: 0,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // Row to display the taskName and taskPriority indicator
-              // this Row displays main task information
-              Row(
-                // crossAxisAlignment: CrossAxisAlignment.end,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    widget.task.taskName,
-                    style: AppStyles.textTileStyle1,
-                  ),
-                  // TODO - work on the priority indicator
-                  // priority indicator
-                  Row(
-                    children: [
-                      CircleAvatar(
-                        backgroundColor: _findPriorityConfig(
-                            widget.task.taskPriority?.name ?? '')?['color'],
-                        radius: 15,
-                        child: Text(
-                          _findPriorityConfig(
-                              widget.task.taskPriority?.name ?? '')?['icon'],
+    return Slidable(
+      startActionPane: ActionPane(
+        extentRatio: 0.3,
+        motion: const ScrollMotion(),
+        children: [
+          // mark task for focus screen
+          SlidableAction(
+            padding: const EdgeInsets.all(0),
+            spacing: 0,
+            onPressed: null,
+            icon: FluentSystemIcons.ic_fluent_target_regular,
+            foregroundColor: AppStyles.unselectedIconColor,
+          )
+        ],
+      ),
+      endActionPane: ActionPane(
+        extentRatio: 0.5,
+        motion: const ScrollMotion(),
+        children: [
+          // delete function >> showDialog with task info and confirm to delete
+          SlidableAction(
+              icon: FluentSystemIcons.ic_fluent_delete_regular,
+              foregroundColor: AppStyles.unselectedIconColor,
+              onPressed: (context) {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) => AlertDialog(
+                    title: const Text('Confirm Delete Task'),
+                    content: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          widget.task.taskName,
+                          style: AppStyles.textTileStyle1,
                         ),
+                        Text(widget.task.taskNote),
+                      ],
+                    ),
+                    actions: [
+                      // delete button
+                      TextButton(
+                        onPressed: () {
+                          // deleteTask(eachTask.id);
+                          Navigator.pop(context);
+                        },
+                        child: const Text('Delete'),
+                      ),
+                      // cancel button
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Text('Cancel'),
                       ),
                     ],
                   ),
-                ],
-              ),
-              // _isExpanded is 'false' show SizedBox = 10, _isExpanded is 'true' show SizedBox = 0.
-              _isExpanded
-                  ? const SizedBox(
-                      height: 10,
-                    )
-                  : const SizedBox(
-                      height: 0,
+                );
+              }),
+          // edit task function
+          SlidableAction(
+            icon: FluentSystemIcons.ic_fluent_edit_regular,
+            foregroundColor: AppStyles.unselectedIconColor,
+            onPressed: null,
+          ),
+          SlidableAction(
+            icon: FluentSystemIcons.ic_fluent_share_ios_regular,
+            foregroundColor: AppStyles.unselectedIconColor,
+            onPressed: null,
+          ),
+        ],
+      ),
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            _isExpanded = !_isExpanded;
+          });
+        },
+        child: Card(
+          margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+            side: BorderSide(
+              width: .5,
+              color: Colors.grey.shade900,
+            ),
+          ),
+          // elevation set to 0
+          elevation: 0,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Row to display the taskName and taskPriority indicator
+                // this Row displays main task information
+                Row(
+                  // crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      widget.task.taskName,
+                      style: AppStyles.textTileStyle1,
                     ),
-              Padding(
-                padding: const EdgeInsets.only(left: 0, right: 0, bottom: 0),
-                child: _isExpanded
-                    // _isExpanded ? is true >> show column
-                    // expands the taskTile to show more information
-                    ? Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const SizedBox(
-                            height: 10,
+                    // TODO - work on the priority indicator
+                    // priority indicator
+                    Row(
+                      children: [
+                        CircleAvatar(
+                          backgroundColor: _findPriorityConfig(
+                              widget.task.taskPriority?.name ?? '')?['color'],
+                          radius: 15,
+                          child: Text(
+                            _findPriorityConfig(
+                                widget.task.taskPriority?.name ?? '')?['icon'],
                           ),
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(left: 10.0, right: 5),
-                            child: Text(
-                              widget.task.taskNote,
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                DateFormat('EEEE, dd MMM yyyy')
-                                    .format(widget.task.dueDate),
-                                style: AppStyles.textTileStyle2,
-                              ),
-                              const SizedBox(
-                                width: 20,
-                              ),
-                              Text(widget.task.taskArea),
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Flexible(
-                                child: Wrap(
-                                  spacing: 4.0,
-                                  runSpacing: 2.0,
-                                  children: widget.task.taskTags.isEmpty
-                                      ? [
-                                          const Text('no tags to display!'),
-                                        ]
-                                      : widget.task.taskTags
-                                          .map(
-                                            (tag) => Chip(
-                                              side: BorderSide.none,
-                                              backgroundColor:
-                                                  Colors.grey.shade400,
-                                              label: Text(tag),
-                                              labelStyle:
-                                                  AppStyles.textTileChipStyle2,
-                                            ),
-                                          )
-                                          .toList(),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                // _isExpanded is 'false' show SizedBox = 10, _isExpanded is 'true' show SizedBox = 0.
+                _isExpanded
+                    ? const SizedBox(
+                        height: 10,
                       )
-                    : null,
-              ),
-            ],
+                    : const SizedBox(
+                        height: 0,
+                      ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 0, right: 0, bottom: 0),
+                  child: _isExpanded
+                      // _isExpanded ? is true >> show column
+                      // expands the taskTile to show more information
+                      ? Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 10.0, right: 5),
+                              child: Text(
+                                widget.task.taskNote,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  DateFormat('EEEE, dd MMM yyyy')
+                                      .format(widget.task.dueDate),
+                                  style: AppStyles.textTileStyle2,
+                                ),
+                                const SizedBox(
+                                  width: 20,
+                                ),
+                                Text(widget.task.taskArea),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Flexible(
+                                  child: Wrap(
+                                    spacing: 4.0,
+                                    runSpacing: 2.0,
+                                    children: widget.task.taskTags.isEmpty
+                                        ? [
+                                            const Text('no tags to display!'),
+                                          ]
+                                        : widget.task.taskTags
+                                            .map(
+                                              (tag) => Chip(
+                                                side: BorderSide.none,
+                                                backgroundColor:
+                                                    Colors.grey.shade400,
+                                                label: Text(tag),
+                                                labelStyle: AppStyles
+                                                    .textTileChipStyle2,
+                                              ),
+                                            )
+                                            .toList(),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        )
+                      : null,
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/screens/inbox_screen/inbox_screen.dart
+++ lib/screens/inbox_screen/inbox_screen.dart
@@ -40,81 +40,9 @@ class _InboxScreenState extends State<InboxScreen> {
         itemBuilder: (context, index) {
           Task eachTask = value.allTasks[index];
           // slidable function >> delete >> edit >> share >> focus
-          return Slidable(
-            startActionPane: ActionPane(
-              extentRatio: 0.3,
-              motion: const ScrollMotion(),
-              children: [
-                // mark task for focus screen
-                SlidableAction(
-                  padding: const EdgeInsets.all(0),
-                  spacing: 0,
-                  onPressed: null,
-                  icon: FluentSystemIcons.ic_fluent_target_regular,
-                  foregroundColor: AppStyles.unselectedIconColor,
-                )
-              ],
-            ),
-            endActionPane: ActionPane(
-              extentRatio: 0.5,
-              motion: const ScrollMotion(),
-              children: [
-                // delete function >> showDialog with task info and confirm to delete
-                SlidableAction(
-                    icon: FluentSystemIcons.ic_fluent_delete_regular,
-                    foregroundColor: AppStyles.unselectedIconColor,
-                    onPressed: (context) {
-                      showDialog(
-                        context: context,
-                        builder: (BuildContext context) => AlertDialog(
-                          title: const Text('Confirm Delete Task'),
-                          content: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Text(
-                                eachTask.taskName,
-                                style: AppStyles.textTileStyle1,
-                              ),
-                              Text(eachTask.taskNote),
-                            ],
-                          ),
-                          actions: [
-                            // delete button
-                            TextButton(
-                              onPressed: () {
-                                deleteTask(eachTask.id);
-                                Navigator.pop(context);
-                              },
-                              child: const Text('Delete'),
-                            ),
-                            // cancel button
-                            TextButton(
-                              onPressed: () {
-                                Navigator.pop(context);
-                              },
-                              child: const Text('Cancel'),
-                            ),
-                          ],
-                        ),
-                      );
-                    }),
-                // edit task function
-                SlidableAction(
-                  icon: FluentSystemIcons.ic_fluent_edit_regular,
-                  foregroundColor: AppStyles.unselectedIconColor,
-                  onPressed: null,
-                ),
-                SlidableAction(
-                  icon: FluentSystemIcons.ic_fluent_share_ios_regular,
-                  foregroundColor: AppStyles.unselectedIconColor,
-                  onPressed: null,
-                ),
-              ],
-            ),
-            child: TaskTile(
-              task: eachTask,
-            ),
+
+          return TaskTile(
+            task: eachTask,
           );
         },
       ),
